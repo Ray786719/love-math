abc582935cc5cc46bf3ec90e29d1a5895b0a7697
Ray786719 <maryum112000@yahoo.com>
2025-08-17 20:05:28 +0100

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runGame() {
+  // Game logic goes here
+}
+function checkAnswer() {
+  // Answer checking logic goes here
+}
+
+function calculateAnswer() {
+  // Answer calculation logic goes here
+}   
+
+function incrementScore() {
+  // Increment score logic goes here
+}
+
+function incrementWrongAnswers() {
+  // Increment wrong answers logic goes here
+}
+
+function displayAdditionQuestion() {
+  // Display addition question logic goes here
+}
+
+function displaySubtractQuestion() {
+  // Display subtraction question logic goes here
+}
+
+function displayMultiplyQuestion() {
+  // Display multiplication question logic goes here
+}
+
+
