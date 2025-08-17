8826f8c6806b63f3e8317b80f96c219d19effdca
Ray786719 <maryum112000@yahoo.com>
2025-08-17 23:40:06 +0100

    displayAdditionQuestion inside function runGame

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,27 @@ for (let button of buttons) {
             alert("You clicked the submit button!");
         } else {
             let gameType = this.getAttribute("data-type");
-            alert("You clicked the " + gameType + " button!");
+            runGame(gameType);
         }
     });
 }
+runGame("addition");
 })
 /** The main game loop called when the script is first loaded
  * and the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
   // Create two random numbers between 1 and 25
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
+
+  if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+  } else {
+    alert("Unknown game type: " + gameType);
+    throw "Unknown game type: " + gameType + ", Aborting!";
+  }
 }
 
 function checkAnswer() {
@@ -36,10 +44,15 @@ function incrementScore() {
 
 function incrementWrongAnswers() {
   // Increment wrong answers logic goes here
+  
+
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
   // Display addition question logic goes here
+document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion() {
